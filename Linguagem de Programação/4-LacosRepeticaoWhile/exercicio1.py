6bcc077451e5328808d1ff793d7ec7be7a788660
Gabriel Antonio Vicente <4555449311@estudante.sed.sc.gov.br>
2026-04-16 16:08:52 -0300

    Delete Linguagem de Programação directory

--- Linguagem de Programação/4-LacosRepeticaoWhile/exercicio1.py
+++
@@ -1,8 +0,0 @@
-numero = 1
-soma = 0
-
-while numero != 0: 
-    numero = int(input("Digite um número: "))
-    soma+=numero
-    if numero == 0:
-        print("A soma de todos os números é: ", soma)
